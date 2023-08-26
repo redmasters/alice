513a1b865bd4d43e17c430f32a174dbac5a25176
Erasmo Jr <erasmo.cjunior@gmail.com>
2023-08-26 14:55:56 -0300

    :wrench: CHORE: Instalado dependencias de documentacao

--- backend/ms-alice/src/main/java/io/red/alice/repositories/CategoryRepository.java
+++ backend/ms-alice/src/main/java/io/red/alice/repositories/CategoryRepository.java
@@ -3,7 +3,7 @@ package io.red.alice.repositories;
 import io.red.alice.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource(collectionResourceRel = "categories", path = "categories")
-public interface CategoryRepository extends JpaRepository<Category, Long> { }
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
