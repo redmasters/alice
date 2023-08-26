4973f5c9be0351016c22375644d5e3018bb7d164
lgshogun <lauro.gpereira@gmail.com>
2023-08-26 16:02:36 -0300

    :construction: PERF: Adicionado o Redis para o uso de memória com cache

--- backend/ms-clientes/app/controllers/clientes_controller.rb
+++ backend/ms-clientes/app/controllers/clientes_controller.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 class ClientesController < ApplicationController
-    before_action :find_cliente, only: :show
+  before_action :find_cliente, only: :show
 
-    def index
-        @clientes = Cliente.all
-        render json: @clientes
-    end
+  def index
+    @clientes = Cliente.all
+    render json: @clientes
+  end
 
-    def show
-        render json: @cliente
-    end
+  def show
+    render json: @cliente
+  end
 
-    private
+  private
 
-    def find_cliente
-        @cliente = Cliente.find(params[:id])
-    end
+  def find_cliente
+    @cliente = Cliente.find(params[:id])
+  end
 end
--- backend/ms-clientes/config/routes.rb
+++ backend/ms-clientes/config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   scope :api do
-      resources :clientes
+    resources :clientes
   end
 end
